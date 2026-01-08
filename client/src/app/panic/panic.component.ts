b2480be51d3f73dab145e1e6c55689212600fbcc
Aman Verma <verma.aman77066055@gmail.com>
2026-01-07 23:04:28 -0600

    Updation of Walkie Talkie v4

--- client/src/app/panic/panic.component.ts
+++ client/src/app/panic/panic.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit, ViewChild, ElementRef, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { EmergencyService, EmergencyContactUser } from '../core/services/emergency.service';
 import { PanicService } from '../core/services/panic.service';
@@ -18,7 +18,7 @@ import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-panic',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   template: `
     <div class="panic-container">
       <div class="container">
